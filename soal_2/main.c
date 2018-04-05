0b80339d95cc754387e874f1ea6f7deab6dca6fc
arielmikiabraham <arielabraham82@gmail.com>
2018-04-05 15:41:27 +0700

    Add keyboard function for moving camera

--- soal_2/main.c
+++ soal_2/main.c
@@ -19,8 +19,7 @@ cell lookAt[6] = {
     {{0.0, 2.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}},
     {{0.0, -2.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}}};
 
-int num = 0;
-int state = 0;
+int num = 0, state = 0, status = 0;
 GLfloat theta;
 GLboolean rotate = GL_TRUE;
 GLuint mainWindow;
@@ -29,12 +28,14 @@ GLuint subWidth = 384, subHeight = 256;
 GLMmodel *model = NULL;
 
 void display();
+void redisplayAll();
 void subDisplay();
-void mainReshape();
-void subReshape();
+void mainReshape(int width, int height);
+void subReshape(int width, int height);
 void drawmodel();
 void redisplayAll();
-void mainMenu();
+void mainMenu(int value);
+void mainKeyboard(unsigned char key, int x, int);
 
 void redisplayAll()
 {
@@ -44,6 +45,39 @@ void redisplayAll()
         glutPostRedisplay();
         subReshape(subWidth, subHeight);
     }
+}
+
+void mainKeyboard(unsigned char key, int x, int y)
+{
+    if (key >= '1' && key <= '6')
+    {
+        status = key - '1';
+    }
+    else
+    {
+        switch (key)
+        {
+        case 'a':
+            lookAt[status].eye[0] -= 0.1;
+            break;
+        case 'd':
+            lookAt[status].eye[0] += 0.1;
+            break;
+        case 'w':
+            lookAt[status].eye[1] += 0.1;
+            break;
+        case 's':
+            lookAt[status].eye[1] -= 0.1;
+            break;
+        case 'q':
+            lookAt[status].eye[2] += 0.1;
+            break;
+        case 'e':
+            lookAt[status].eye[2] -= 0.1;
+            break;
+        }
+    }
+    redisplayAll();
 }
 
 void mainMenu(int value)
@@ -74,7 +108,6 @@ void drawmodel(void)
 
 void display()
 {
-    glClearColor(0.8, 0.8, 0.8, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glutSwapBuffers();
 }
@@ -87,7 +120,7 @@ void subDisplay()
         theta = 360.0 * state / 1000;
     }
 
-    glClearColor(0.6, 0.6, 0.6, 0.0);
+    glClearColor(0.0, 0.8, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
     glRotatef(theta, 0.0f, 1.0f, 0.0f);
@@ -98,19 +131,21 @@ void subDisplay()
 
 void subReshape(int width, int height)
 {
+    glClearColor(0.0, 0.8, 0.0, 0.0);
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(60.0, (GLfloat)width / height, 1.0, 256.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glClearColor(0.6, 0.6, 0.6, 0.0);
     gluLookAt(lookAt[num].eye[0], lookAt[num].eye[1], lookAt[num].eye[2],
               lookAt[num].at[0], lookAt[num].at[1], lookAt[num].at[2],
               lookAt[num].up[0], lookAt[num].up[1], lookAt[num].up[2]);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
     if (++num == 6)
     {
         num = 0;
@@ -147,6 +182,7 @@ int main(int argc, char *argv[])
     mainWindow = glutCreateWindow("CAR CCTV");
     glutDisplayFunc(display);
     glutReshapeFunc(mainReshape);
+    glutKeyboardFunc(mainKeyboard);
 
     for (int i = 0; i < 6; i++)
     {
@@ -156,6 +192,7 @@ int main(int argc, char *argv[])
                                            subWidth, subHeight);
         glutDisplayFunc(subDisplay);
         glutReshapeFunc(subReshape);
+        glutKeyboardFunc(mainKeyboard);
         glutCreateMenu(mainMenu);
         glutAddMenuEntry("Toggle Rotation", 'r');
         glutAttachMenu(GLUT_RIGHT_BUTTON);
